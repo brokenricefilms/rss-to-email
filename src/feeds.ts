ab9c4d22c386bf91acef1622e7e09de74b712206
Broken Rice Films <brokenricefilms@gmail.com>
2024-05-16 04:22:54 -0700

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,5 @@
 export const feeds = [
   // Daring Fireball
-  'https://daringfireball.net/feeds/main',
+  'https://www.upwork.com/ab/feed/jobs/rss?amount=40-&ontology_skill_uid=1031626793223176192&paging=0-10&payment_verified=1&proposals=0-4%2C5-9&q=NOT%20Automation&sort=recency&t=1&api_params=1&securityToken=34408095ab25678a2024d37cb7a31922a7f3c60b617b38301086ad907b49281b3fe1862ed0a272e4418fd7a17c3bb892cf0fb61fc924f8c981d126bb5b42fad4&userUid=1772236613276483584&orgUid=1772236613276483585',
 
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
-
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
 ]

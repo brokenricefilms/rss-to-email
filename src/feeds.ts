625b8aea364e72d3a1646ec73cf432f0de8eb041
Broken Rice Films <brokenricefilms@gmail.com>
2024-06-28 09:41:17 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,3 +1,6 @@
 export const feeds = [
-    'https://www.upwork.com/ab/feed/jobs/rss?paging=NaN-undefined&q=video%20editing%20travel&sort=recency&api_params=1&securityToken=34408095ab25678a2024d37cb7a31922a7f3c60b617b38301086ad907b49281b3fe1862ed0a272e4418fd7a17c3bb892cf0fb61fc924f8c981d126bb5b42fad4&userUid=1772236613276483584&orgUid=1772236613276483585',
+    // travel
+    // 'https://www.upwork.com/ab/feed/jobs/rss?paging=NaN-undefined&q=video%20editing%20travel&sort=recency&api_params=1&securityToken=34408095ab25678a2024d37cb7a31922a7f3c60b617b38301086ad907b49281b3fe1862ed0a272e4418fd7a17c3bb892cf0fb61fc924f8c981d126bb5b42fad4&userUid=1772236613276483584&orgUid=1772236613276483585',
+    // vlog
+'https://www.upwork.com/ab/feed/jobs/rss?paging=NaN-undefined&q=editing%20vlog&sort=recency&api_params=1&securityToken=34408095ab25678a2024d37cb7a31922a7f3c60b617b38301086ad907b49281b3fe1862ed0a272e4418fd7a17c3bb892cf0fb61fc924f8c981d126bb5b42fad4&userUid=1772236613276483584&orgUid=1772236613276483585'
 ]
